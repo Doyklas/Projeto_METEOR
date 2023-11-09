77db23d78c82662baea0833880e9062767751c55
Douglas Henrique <132010791+Doyklas@users.noreply.github.com>
2023-11-09 17:12:34 -0300

    Add files via upload

---
+++ usinagem_meteor_.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE usinagem_meteor_;
+use usinagem_meteor_;
+
+-- entidades fortes
+CREATE TABLE maquina(
+	pk_id_maq 		INT NOT NULL,
+    nome_maq 		VARCHAR(100) NOT NULL,
+    descricao_maq 	VARCHAR(100) NOT NULL,
+);
+CREATE TABLE operador(
+
+);
+CREATE TABLE fornecedor(
+
+);
